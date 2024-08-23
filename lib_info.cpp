b50e92797282734628a14b6f029373cddeeecf56
Mia Patrikios <mpatrikios@icloud.com>
2024-08-23 09:16:47 -0400

    added bool

--- lib_info.cpp
+++ lib_info.cpp
@@ -12,13 +12,25 @@ struct songInfo{
     int trackNumber;
     string songName;
     string songTime; //in seconds
+
+    bool operator<(const songInfo& other) const {
+        return trackNumber < other.trackNumber;
+    }
 };
 
 // total album time
-string total_time(set<songInfo> songs){}
+// string total_time(set<songInfo> songs){}
 
-// convert song time to seconds
-void convertToSeconds(string songTime){}
+// // convert song time to seconds
+// void convertToSeconds(string songTime){}
+
+void removeUnderscores(string &tempLine){
+    for(int i = 0; i < tempLine.size(); i++){
+        if(tempLine[i] == '_'){
+            tempLine[i] = ' ';
+        }
+    }
+}
 
 int main(int argc, char *argv[]){
     string inputFile;
@@ -26,26 +38,26 @@ int main(int argc, char *argv[]){
     ifstream file(inputFile);
 
     string tempLine, artistName, genre;
-    map <string, set<songInfo>> lib_info;
+    map<string, set<songInfo > > lib_info;
 
     while(getline(file, tempLine)){
-        //replace underscores with space
-        for(int i = 0; i < tempLine.size(); i++){
-            if(tempLine[i] == '_'){
-                tempLine[i] = ' ';
-            }
-        }
 
         songInfo tempSong;
         stringstream ss(tempLine);
         ss >> tempSong.songName;
-        ss >> tempSong.albumName;
+        ss >> tempSong.songTime;
         ss >> artistName; //key
         ss >> tempSong.albumName;
         ss >> genre; //don't need
         ss >> tempSong.trackNumber;
 
+        removeUnderscores(tempSong.songName);
+        removeUnderscores(artistName);
+        removeUnderscores(tempSong.albumName);
+
         lib_info[artistName].insert(tempSong);
+
+        // cout << tempSong.songName << " " << tempSong.albumName << " " << artistName << " " << tempSong.albumName  << " " << tempSong.trackNumber << " " << tempSong.songTime << endl;
 
     }
 
@@ -54,21 +66,21 @@ int main(int argc, char *argv[]){
         const set<songInfo> songs = artistPair.second; //access the set of stongs stored as the value
 
         //print artist name, number of songs, and total album time
-        cout << artist << ": " << songs.size() << "Total time: "
-             << totalTime(songs) << endl;
+        cout << artist << ": " << songs.size() << "Total time: " << endl;
+            //  << totalTime(songs) << endl;
 
         string albumName = songs.begin()->albumName; //assuming no artist will have more than one album
 
         //creates a new map that sorts songs by album name under the same artist
         //dont know if necessary
-        // map<string, set<songInfo>> albumMap;
-        // for (const auto& song : songs) {
-        //     albumMap[song.albumName].insert(song);
-        // }
+        //map<string, set<songInfo>> albumMap;
+        //for (const auto& song : songs) {
+        //    albumMap[song.albumName].insert(song);
+       // }
 
         //print album name, number of songs, and total album time
-        cout << "  " << albumName << ": " << songs.size() << "Total time: "
-            << totalTime(songs) << "" << endl; 
+        cout << "  " << albumName << ": " << songs.size() << "Total time: " << endl;
+            // << totalTime(songs) << "" << endl; 
 
         //another for loop, print every song in the set with its track number and time
         for (auto song : songs) {
